efdbc7c91d99c55f23865b666e435eee1fac29aa
unknown <kristijonas.karalaitis@gmail.com>
2021-09-18 20:21:44 +0300

    created signup

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { generateAccessToken} = require('../middleware/authentication');
+const bcrypt = require('bcrypt')
 
 const router = new express.Router();
 
@@ -33,29 +34,45 @@ const users = [
     }
 ]
 
-router.post('/users/signup', (req, res) => {
-    //authentication
-
-    const username = req.body.username
-
-    const user = {
-        name: username
+router.post('/users/signup', async (req, res) => {
+    const userDetails = {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: req.body.password,
     }
-    console.log(user)
 
-    const accessToken = generateAccessToken(user)
-
-    res.json({accessToken: accessToken})
+    try {
+        const hashedPassword = await bcrypt.hash(userDetails.password, 10)
+        const newUser = {
+            name: userDetails.name,
+            surname: userDetails.surname,
+            email: userDetails.email,
+            password: hashedPassword,
+        }
+        users.push(newUser)
+        res.status(201).send('user ' + userDetails.name + ' created')
+    } catch {
+        res.status(500).json({error: 'error when creating a user'}).send()
+    }
 })
 
-router.post('/users/login', (req, res) => {
-    //authentication
-
-    const username = req.body.username
-
-    const user = {
-        name: username
+router.post('/users/login', async (req, res) => {
+    const email = req.body.email 
+    const password = req.body.password
+    const user = users.find(user => user.email === email)
+    if (user == null) {
+        return res.status(400).json({error: 'Cannot find user'}).send()
     }
+    try {
+        const passwordsMatch = await bcrypt.compare(password, user.password)
+        if (!passwordsMatch) {
+            return res.status(400).json({error: 'Incorrect password'})
+        }
+    } catch {
+        return res.status(500).json({error: 'Cannot dehash password'})
+    }
+
     console.log(user)
 
     const accessToken = generateAccessToken(user)
