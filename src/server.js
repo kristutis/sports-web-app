0b3bf15a1f5cac91788d894c213600e4306c64bf
unknown <kristijonas.karalaitis@gmail.com>
2021-09-18 18:11:29 +0300

    moving to packages

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '../config/.env')})
 const express = require('express')
 const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
+
+const TOKEN_EXPIRE = process.env.TOKEN_EXPIRE || '10m'
+const PORT = process.env.PORT || 8000;
+ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || ''
+if (!ACCESS_TOKEN_SECRET) {
+    throw Error('Access token secret not found!')
+}
 
 const posts = [
     {
@@ -22,6 +30,10 @@ app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
+// app.delete('/logout', (req, res) => {
+//     jwt.
+// })
+
 app.post('/login', (req, res) => {
     //authentication
 
@@ -32,18 +44,24 @@ app.post('/login', (req, res) => {
     }
     console.log(user)
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = generateAccessToken(user)
+
     res.json({accessToken: accessToken})
 })
 
+function generateAccessToken(user) {
+    return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: TOKEN_EXPIRE })
+}
+
 function authenticateToken(req, res, next) {
+    //generate new token
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) {
         return res.sendStatus(401)   
     }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
             return res.sendStatus(403) //token no longer valid
         }
@@ -52,4 +70,6 @@ function authenticateToken(req, res, next) {
     })
 }
 
-app.listen(3000); 
+app.listen(PORT, () => {
+    console.log('Server is up! Listening PORT: ' + PORT);
+});
