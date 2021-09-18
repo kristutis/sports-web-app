5206d7c79ad2192acc59d26f830695c3ad18e0c4
unknown <kristijonas.karalaitis@gmail.com>
2021-09-19 01:20:44 +0300

    moved env file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //pervadint pathus i /api/
-const path = require('path')
-require('dotenv').config({path: path.resolve(__dirname, '../config/.env')})
+require('dotenv').config();
 const express = require('express')
 
 const app = express()
