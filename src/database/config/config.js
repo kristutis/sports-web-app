b130204b690d705f7a9d9a64f21df929c469b8ed
kristutis <kristijonas.karalaitis@gmail.com>
2021-09-28 22:37:40 +0300

    added gcloud db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    password: '',
-    user: 'root',
-    database: 'sports-db',
-    host: 'localhost',
+    password: 'sports-db-pass-1',
+    user: 'sports-db',
+    database: 'sports_db',
+    host: '34.88.169.216',
     port: '3306'
 })
 
