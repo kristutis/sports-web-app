015c15da0fd0161d3d6e4fa193c4260823056fd2
kristutis <kristijonas.karalaitis@gmail.com>
2021-09-29 21:31:46 +0300

    connects to db when deployed

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,8 +5,9 @@ const db = mysql.createPool({
     password: 'sports-db-pass-1',
     user: 'sports-db',
     database: 'sports_db',
-    host: '34.88.169.216',
-    port: '3306'
+    // host: '34.88.169.216',
+    socketPath: '/cloudsql/cosmic-quarter-326513:europe-north1:sports-db',
+    // port: '3306'
 })
 
 module.exports = db
